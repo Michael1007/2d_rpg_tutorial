ca41f3fa28c9b07901e580136d68ee288ef3be25
Mnunno <michael@nunno.com>
2025-03-17 20:36:26 -0400

    Update player.gd
    Video tutorial 2 complete

--- Player/player.gd
+++ Player/player.gd
@@ -6,8 +6,9 @@ extends CharacterBody2D
 # var b = "text"
 
 # When ACCELERATION was 200, it felt like we were on ice. Could be useful
-const ACCELERATION = 200
+const ACCELERATION = 10
 const MAX_SPEED = 100
+const FRICTION = 10
 
 # Velocity = the x and y position combined
 var vel = Vector2.ZERO
@@ -17,18 +18,22 @@ var vel = Vector2.ZERO
 	#print("Hello world.")
 
 # Look at "Search help" in top bar to see what this function does.
+# "delta" is used for real world time, NOT framerate
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	
+	# multipy delta whenver you have something that changes over time
 	if input_vector != Vector2.ZERO:
 		vel += input_vector * ACCELERATION * delta
+		vel = vel.limit_length(MAX_SPEED * delta)
 	else:
-		vel = Vector2.ZERO
-		
-	move_and_collide(vel * delta)
+		vel = vel.move_toward(Vector2.ZERO, FRICTION * delta)
+	
+	#print(vel)
+	move_and_collide(vel)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
