8b4e83df2545fae61e7833a9e00d8bba72477c41
Mnunno <michael@nunno.com>
2025-03-19 18:07:36 -0400

    Video 9 - finish
    State Machine creation for attack versus move animations

--- Player/player.gd
+++ Player/player.gd
@@ -10,6 +10,15 @@ const ACCELERATION = 500
 const MAX_SPEED = 80
 const FRICTION = 500
 
+# enumeration: setting "variables" that cannot change. 
+# automatically created with values (0, 1, 2, etc.)
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
 # Velocity = the x and y position combined
 var vel = Vector2.ZERO
 
@@ -18,7 +27,19 @@ var vel = Vector2.ZERO
 @onready var animationTree = $AnimationTree
 @onready var animationState = animationTree.get("parameters/playback") # Stores what we hvae set up in root, can access that info
 
+func _ready():
+	animationTree.active = true
+
 func _physics_process(delta):
+	match state: # acts like a switch statement
+		MOVE:
+			move_state(delta) # beginning of a state machine (only one block of code is run at once
+		ROLL:
+			pass
+		ATTACK:
+			attack_state(delta)
+
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -28,6 +49,10 @@ func _physics_process(delta):
 	if input_vector != Vector2.ZERO:
 		animationTree.set("parameters/Idle/blend_position", input_vector) # set blend position for idle
 		animationTree.set("parameters/Run/blend_position", input_vector) # set blend position for run
+		animationTree.set("parameters/Attack/blend_position", input_vector) 
+		# ^^^NOT in attack state because our input vector is not in attack state
+		# also we dont want them to change direction of attack mid animation
+		
 		animationState.travel("Run")
 		vel = vel.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
@@ -38,3 +63,13 @@ func _physics_process(delta):
 
 	move_and_slide()
 	vel = velocity
+	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+	
+func attack_state(delta):
+	velocity = Vector2.ZERO # so they don't move with friction when attack
+	animationState.travel("Attack")
+	
+func attack_animation_finish():
+	state = MOVE
