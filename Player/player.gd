68b158ca9352a6133794712476ddf8a47d48d46c
Mnunno <michael@nunno.com>
2025-03-19 12:17:33 -0400

    Video 6 - Finish
    All about movement animation and animation trees

--- Player/player.gd
+++ Player/player.gd
@@ -15,9 +15,9 @@ var vel = Vector2.ZERO
 
 # Once the game is ready, this will be set up and ready to go (an "onready" variable)
 @onready var animationPlayer = $AnimationPlayer # '$' is shorthand for path to a node, which is in the same scene
+@onready var animationTree = $AnimationTree
+@onready var animationState = animationTree.get("parameters/playback") # Stores what we hvae set up in root, can access that info
 
-# Look at "Search help" in top bar to see what this function does.
-# "delta" is used for real world time, NOT framerate
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -26,16 +26,15 @@ func _physics_process(delta):
 	
 	# multipy delta whenver you have something that changes over time
 	if input_vector != Vector2.ZERO:
-		if input_vector.x > 0:
-			animationPlayer.play("RunRight") # in this if statement is when we are moving
-		else:
-			animationPlayer.play("RunLeft")
+		animationTree.set("parameters/Idle/blend_position", input_vector) # set blend position for idle
+		animationTree.set("parameters/Run/blend_position", input_vector) # set blend position for run
+		animationState.travel("Run")
 		vel = vel.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
-		animationPlayer.play("IdleRight")
+		animationState.travel("Idle")
 		vel = vel.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	velocity = vel
-	#print(vel)
+
 	move_and_slide()
 	vel = velocity
